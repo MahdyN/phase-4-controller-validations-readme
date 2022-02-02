a00b7585f89e0af2493c4a9579fbf098dee831aa
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2022-02-02 09:12:57 -0500

    commit tree ca2054d2e96744f800f1dce810c3f4689e066a30

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   # GET /birds
   def index
     birds = Bird.all
@@ -9,7 +9,7 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params) # We use the bang operator counterpart of this method because it will raise an exception(ActiveRecord::RecordInvalid) if an invalid instance is being attempted to be created, this helps us with error handling so we can use the rescue method and minimize the amount of code written(no if/else)
     render json: bird, status: :created
   end
 
@@ -22,8 +22,8 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
-    render json: bird
+    bird.update!(bird_params)
+    render json: bird, status: :accepted
   end
 
   # DELETE /birds/:id
@@ -46,5 +46,11 @@ class BirdsController < ApplicationController
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
   end
+
+  # Exception handling for invalid records, invalid variable is an instance of the exception itself which contains the errors for the invalid record
+
+  def render_unprocessable_entity_response(invalid)
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end 
 
 end
